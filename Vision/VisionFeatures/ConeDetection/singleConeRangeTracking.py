656cfd197f349add5dbdc57689d7c0e693e206d7
Huy H. Nguyen <huynguyen120390@gmail.com>
2017-06-30 16:16:43 -0700

    Single Orange Cone Distance Tracking

---
+++ Vision/VisionFeatures/ConeDetection/singleConeRangeTracking.py
@@ -0,0 +1,108 @@
+# import the necessary packages
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+#comment or uncomment the following lines for detecting greens 
+#greenLower = (29, 86, 6)
+#greenUpper = (64, 255, 255)
+
+orangeLower = (179,198,255)
+orangerUpper = (0,64,255)
+pts = deque(maxlen=args["buffer"])
+ 
+# if a video path was not supplied, grab the reference
+# to the webcam
+if not args.get("video", False):
+	camera = cv2.VideoCapture(0)
+ 
+# otherwise, grab a reference to the video file
+else:
+	camera = cv2.VideoCapture(args["video"])
+	
+# keep looping
+while True:
+	# grab the current frame
+	(grabbed, frame) = camera.read()
+ 
+	# if we are viewing a video and we did not grab a frame,
+	# then we have reached the end of the video
+	if args.get("video") and not grabbed:
+		break
+ 
+	# resize the frame, blur it, and convert it to the HSV
+	# color space
+	#frame = imutils.resize(frame, width=600)
+	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+ 
+	# construct a mask for the color "green", then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	#mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, orangeLower, orangeUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the cone
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+ 
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+                c = max(cnts, key=cv2.contourArea)
+                x,y,w,h = cv2.boundingRect(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))	
+                if h > 10:
+			# draw the rectangle enclosing the ,
+			# then update the list of tracked points
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(204,0,102),2)
+                        print ("height",h)
+                        #With focal length is 0.00576m
+                        distance = (3.04 * 0.19304)/(h*0.00576)
+                        #With pixel dimension is 0.00112mm
+			#distance = (0.591065 * 0.19304)/(radius*2*0.00112) 
+                        print("distance", distance)
+                
+	# update the points queue
+	pts.appendleft(center)
+
+	# loop over the set of tracked points
+	for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0,153,204), thickness)
+ 
+	# show the frame to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+ 
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
